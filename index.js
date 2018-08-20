53bb7a6a8135f4f1006700680c7d46af129efb82
Oliver de Sousa <oliver.seq@gmail.com>
2018-08-20 09:22:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ function prependKitten(Arnold){
   return Arnold
 }
 
-<<<<<<< HEAD
 function removeLastKitten(Garfield){
   Garfield = kittens.slice(0,2);
   return Garfield
@@ -36,9 +35,9 @@ function removeLastKitten(Garfield){
 function removeFirstKitten(Milo){
   Milo = kittens.slice(1);
   return Milo
-=======
+}
+
 function removeLastKitten(kittens){
   kittens = kittens.slice(1);
   return kittens
->>>>>>> 67b1521a5e838b18d986e0aa8bd9c1b885b7f59c
 }
